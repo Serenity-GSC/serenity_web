64ff19aec0db7c61d9a0827b7ff3a060b881d28c
Zidane Arkan <zidark897@gmail.com>
2024-02-09 12:04:18 +0700

    Add navbar and hero page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,38 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, Router, Event, NavigationEnd } from '@angular/router';
+import { IStaticMethods } from 'preline/preline';
+import { NavbarComponent } from './navbar/navbar.component';
+import { HeaderComponent } from './home/header/header.component';
+
+declare global {
+  interface Window {
+    HSStaticMethods: IStaticMethods;
+  }
+}
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet, 
+    NavbarComponent, 
+    HeaderComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'serenity_web';
+  constructor(private router: Router) {
+    window.HSStaticMethods = window.HSStaticMethods || {};
+  }
+  ngOnInit() {
+    this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        setTimeout(() => {
+          window.HSStaticMethods.autoInit();
+        }, 100);
+      }
+    });
+  }
 }
