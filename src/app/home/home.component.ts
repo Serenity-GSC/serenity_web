e7da09d384c968a5a4dc553c2b7953b199943afa
Zidane Arkan <zidark897@gmail.com>
2024-02-12 13:58:13 +0700

    Add blog and testimonials sections

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { AboutusComponent } from './aboutus/aboutus.component';
 import { OurfeatureComponent } from './ourfeature/ourfeature.component';
+import { TestimonialsComponent } from './testimonials/testimonials.component';
 
 @Component({
   selector: 'app-home',
@@ -10,6 +11,7 @@ import { OurfeatureComponent } from './ourfeature/ourfeature.component';
     HeaderComponent,
     AboutusComponent,
     OurfeatureComponent,
+    TestimonialsComponent,
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
