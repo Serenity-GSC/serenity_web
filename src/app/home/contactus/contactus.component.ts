b2c7774ab0222be9b0f7ad5e2390707fc52846cf
Zidane Arkan <zidark897@gmail.com>
2024-02-20 15:21:48 +0700

    Add notification page

--- src/app/home/contactus/contactus.component.ts
+++ src/app/home/contactus/contactus.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import emailjs, { type EmailJSResponseStatus } from '@emailjs/browser';
 
 @Component({
   selector: 'app-contactus',
@@ -8,5 +9,28 @@ import { Component } from '@angular/core';
   styleUrl: './contactus.component.css'
 })
 export class ContactusComponent {
-
+  public sendEmail(e: Event) {
+    e.preventDefault();
+    console.log(e.target as HTMLFormElement);
+    emailjs
+        .sendForm(
+          'serenity_contactus', 
+          'template_0mec3ea', 
+          e.target as HTMLFormElement, {
+          publicKey: '_h4TzUACZS_bJ8p3g',
+        })
+        .then(()=>{
+          console.log('Email sent successfully!');
+        }, (error) => {
+          console.error('Email sent failed:', error);
+        });
+  }
 }
+
+// emailjs.send("serenity_contactus","template_0mec3ea",{
+//   subject: "New Goals",
+//   first_name: "Muhammad",
+//   last_name: "Arkan",
+//   message: "I want to inform you about a new deals",
+//   email: "jokerlex30@gmail.com",
+//   });
